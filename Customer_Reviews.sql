b99c628030a181a333b006aed1c27edba123c972
luffy6281 <72807559+luffy6281@users.noreply.github.com>
2025-02-06 19:54:40 +0530

    Customer Reviews

---
+++ Customer_Reviews.sql
@@ -0,0 +1,12 @@
+-- Query to clean whitespace issues in the ReviewText column
+
+SELECT 
+    ReviewID, 
+    CustomerID,  
+    ProductID, 
+    ReviewDate, 
+    Rating,
+    REPLACE(ReviewText, '  ', ' ') AS ReviewText
+  
+FROM 
+    dbo.customer_reviews; 
